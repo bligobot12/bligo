a5632cbb9a422e7fac490500db5e8c233e640aef
Mac <mac@LMs-Mac-mini.local>
2026-03-08 16:10:44 -0400

    Add /settings /connections /profile to middleware matcher

--- middleware.js
+++ middleware.js
@@ -31,7 +31,7 @@ export async function middleware(request) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user && (request.nextUrl.pathname.startsWith('/home') || request.nextUrl.pathname.startsWith('/onboarding'))) {
+  if (!user && (request.nextUrl.pathname.startsWith('/home') || request.nextUrl.pathname.startsWith('/onboarding') || request.nextUrl.pathname.startsWith('/settings') || request.nextUrl.pathname.startsWith('/connections') || request.nextUrl.pathname.startsWith('/profile'))) {
     const loginUrl = new URL('/login', request.url);
     return NextResponse.redirect(loginUrl);
   }
@@ -40,5 +40,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/home/:path*', '/onboarding/:path*', '/messages/:path*'],
+  matcher: ['/home/:path*', '/onboarding/:path*', '/messages/:path*', '/settings/:path*', '/connections/:path*', '/profile/:path*'],
 };
